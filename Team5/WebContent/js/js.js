1ff1ae0fedb0ee88113987a077410a1058398a59
user <user@DESKTOP-COJ9858>
2020-07-23 15:28:19 +0900

    1.0.3_서월성(2020-07-23)

---
+++ Team5/WebContent/js/js.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('.h_board').click(function(){
+		location.href = './BoardList.do';
+	});
+	
+	$('.h_check').click(function(){
+		location.href = './CheckList.do';
+	});
+});
